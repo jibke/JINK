5e5b1e2cb79dc5905827d022db6f6ec6ed0a3a9b
金科 <49086801+jibke@users.noreply.github.com>
2019-04-22 15:52:53 +0800

    Add files via upload

---
+++ 1.13.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{
+    int x,y;
+    puts("请输入两个整数:")；
+    printf("输入x:");  scanf("%d",&x);
+    printf("输入y:");  scanf("%d",&y);
+    printf("两个整数的和为%d,\n",x+y);
+    return 0;
+}
