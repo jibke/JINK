c2ba5fe0e55967bb0003c0823b95e1dafe1ff53b
金科 <49086801+jibke@users.noreply.github.com>
2019-05-29 17:03:57 +0800

    Add files via upload

---
+++ 2.1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)
+{
+    int x,y;
+    puts("输入两个整数");
+    printf("整数x:");scanf("%d",&x);
+    printf("整数y:");scanf("%d",&y);
+    printf("x+y=%d\n",x+y);
+    printf("x-y=%d\n",x-y);
+    printf("x*y=%d\n",x*y);
+    printf("x/y=%d\n",x/y);
+    printf("x%y=%d\n",x%y);
+    return 0;
+}
+
