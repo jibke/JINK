c2082926f6cb7c4b00b612414a57a3c8fd4dafbd
金科 <49086801+jibke@users.noreply.github.com>
2019-05-29 17:06:37 +0800

    Add files via upload

---
+++ e2.2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{
+    int x,y;
+    puts("输入两个整数.");
+    printf("整数x:");scanf("%d",&x);
+    printf("整数y:");scanf("%d",&y);
+    printf("它们的和为%d,积是%d",x+y,x*y);
+    return 0;
+}
